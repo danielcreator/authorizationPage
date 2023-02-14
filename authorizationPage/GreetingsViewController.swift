16cff44ea73daf3ce7cdd528b5edb5bafe241d0c
Daniel <danielgonsales93@gmail.com>
2023-02-14 18:08:50 +0300

    adding background gradient

--- authorizationPage/GreetingsViewController.swift
+++ authorizationPage/GreetingsViewController.swift
@@ -16,7 +16,13 @@ final class GreetingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         userName.text = usernameField
+        let colorTop = UIColor(red: 0.87, green: 0.37, blue: 0.54, alpha: 1).cgColor
+        let colorBottom = UIColor (red: 0.97, green: 0.73, blue: 0.33, alpha: 1).cgColor
         
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = self.backgroundView.bounds
+        gradientLayer.colors = [colorTop, colorBottom]
+        self.backgroundView.layer.insertSublayer(gradientLayer, at: 0)
     }
     @IBAction func logoutButtonTapped() {
         dismiss(animated: true)
